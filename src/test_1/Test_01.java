ffe98bd077812f33f1fed5ae0933e527fb324bf6
GD <nagundo@naver.com>
2025-02-27 16:42:28 +0900

    test commit 1

--- src/test_1/Test_01.java
+++ src/test_1/Test_01.java
@@ -4,6 +4,9 @@ public class Test_01 {
 
 	public static void main(String[] args) {
 		System.out.print("wewfdwe");		
+		System.out.print("wewfdwe");		
+		System.out.print("wewfdwe");		
+		System.out.print("wewfdwe");		
 	}
 
 }
